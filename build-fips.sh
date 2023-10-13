c3f2f5118f7c6014808c1aacb68013841088ffe7
Vishwanath Taykhande <vishu2498@gmail.com>
2023-10-13 09:32:32 +0530

    PCP-1955 Update Dockerfiles, scripts and go.mod

--- build-fips.sh
+++ build-fips.sh
@@ -2,6 +2,7 @@
 
 VERSION="spectro-v1.11.0"
 IMG_REPO="gcr.io/spectro-images-public/release/jetstack/cert-manager"
+BUILDER_GOLANG_VERSION="1.21"
 
 ## dev repo ## 
 #IMG_REPO="gcr.io/spectro-dev-public/${USER}/cert-manager"
@@ -11,19 +12,19 @@ make _bin/scratch/cert-manager.license
 
 docker buildx create --use
 
-docker buildx build --push \
+docker buildx build --build-arg BUILDER_GOLANG_VERSION=${BUILDER_GOLANG_VERSION} --build-arg CRYPTO_LIB=${FIPS_ENABLE} --push \
 --platform linux/arm64,linux/amd64 \
 --tag ${IMG_REPO}/cert-manager-controller:${VERSION}-fips . -f ./Dockerfile.fips --target controller
 
-docker buildx build --push \
+docker buildx build --build-arg BUILDER_GOLANG_VERSION=${BUILDER_GOLANG_VERSION} --build-arg CRYPTO_LIB=${FIPS_ENABLE} --push \
 --platform linux/arm64,linux/amd64 \
 --tag ${IMG_REPO}/cert-manager-webhook:${VERSION}-fips . -f ./Dockerfile.fips --target webhook
 
-docker buildx build --push \
+docker buildx build --build-arg BUILDER_GOLANG_VERSION=${BUILDER_GOLANG_VERSION} --build-arg CRYPTO_LIB=${FIPS_ENABLE} --push \
 --platform linux/arm64,linux/amd64 \
 --tag ${IMG_REPO}/cert-manager-cainjector:${VERSION}-fips . -f ./Dockerfile.fips --target cainjector
 
-docker buildx build --push \
+docker buildx build --build-arg BUILDER_GOLANG_VERSION=${BUILDER_GOLANG_VERSION} --build-arg CRYPTO_LIB=${FIPS_ENABLE} --push \
 --platform linux/arm64,linux/amd64 \
 --tag ${IMG_REPO}/cert-manager-acmesolver:${VERSION}-fips . -f ./Dockerfile.fips --target acmesolver
 
--- build-nonfips.sh
+++ build-nonfips.sh
@@ -1,29 +1,30 @@
 #!/bin/sh
 
 VERSION="spectro-v1.11.0"
-#IMG_REPO="gcr.io/spectro-images-public/release/jetstack/cert-manager"
+IMG_REPO="gcr.io/spectro-images-public/release/jetstack/cert-manager"
+BUILDER_GOLANG_VERSION="1.21"
 
 ## dev repo ## 
-IMG_REPO="gcr.io/spectro-dev-public/${USER}/cert-manager"
+#IMG_REPO="gcr.io/spectro-dev-public/${USER}/cert-manager"
 
 make _bin/scratch/cert-manager.licenses_notice
 make _bin/scratch/cert-manager.license
 
 docker buildx create --use
 
-docker buildx build --push \
+docker buildx build --build-arg BUILDER_GOLANG_VERSION=${BUILDER_GOLANG_VERSION} --push \
 --platform linux/arm64,linux/amd64 \
 --tag ${IMG_REPO}/cert-manager-controller:${VERSION} . -f ./Dockerfile.nonfips --target controller
 
-docker buildx build --push \
+docker buildx build --build-arg BUILDER_GOLANG_VERSION=${BUILDER_GOLANG_VERSION} --push \
 --platform linux/arm64,linux/amd64 \
 --tag ${IMG_REPO}/cert-manager-webhook:${VERSION} . -f ./Dockerfile.nonfips --target webhook
 
-docker buildx build --push \
+docker buildx build --build-arg BUILDER_GOLANG_VERSION=${BUILDER_GOLANG_VERSION} --push \
 --platform linux/arm64,linux/amd64 \
 --tag ${IMG_REPO}/cert-manager-cainjector:${VERSION} . -f ./Dockerfile.nonfips --target cainjector
 
-docker buildx build --push \
+docker buildx build --build-arg BUILDER_GOLANG_VERSION=${BUILDER_GOLANG_VERSION} --push \
 --platform linux/arm64,linux/amd64 \
 --tag ${IMG_REPO}/cert-manager-acmesolver:${VERSION} . -f ./Dockerfile.nonfips --target acmesolver
 
