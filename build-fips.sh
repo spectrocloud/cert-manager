110d889f3bea4d95b2b3e4e63fc0e9e89ec8ee01
Jayesh Srivastava <jayesh0200@gmail.com>
2023-11-21 16:05:39 +0530

    Update build scripts for version

--- build-fips.sh
+++ build-fips.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-SPECTRO_VERSION="4.1.0"
 VERSION="v1.3.1-spectro-"
 if [[ $FIPS_REGISTRY ]]; then
   IMG_REPO=$FIPS_REGISTRY
@@ -19,17 +18,17 @@ docker buildx create --use
 
 docker buildx build --build-arg BUILDER_GOLANG_VERSION=${BUILDER_GOLANG_VERSION} --build-arg CRYPTO_LIB=${FIPS_ENABLE} --push \
 --platform linux/arm64,linux/amd64 \
---tag ${IMG_REPO}/cert-manager-controller:${VERSION}-${SPECTRO_VERSION} . -f ./Dockerfile.fips --target controller
+--tag ${IMG_REPO}/cert-manager-controller:${VERSION}-$SPECTRO_VERSION . -f ./Dockerfile.fips --target controller
 
 docker buildx build --build-arg BUILDER_GOLANG_VERSION=${BUILDER_GOLANG_VERSION} --build-arg CRYPTO_LIB=${FIPS_ENABLE} --push \
 --platform linux/arm64,linux/amd64 \
---tag ${IMG_REPO}/cert-manager-webhook:${VERSION}-${SPECTRO_VERSION} . -f ./Dockerfile.fips --target webhook
+--tag ${IMG_REPO}/cert-manager-webhook:${VERSION}-$SPECTRO_VERSION . -f ./Dockerfile.fips --target webhook
 
 docker buildx build --build-arg BUILDER_GOLANG_VERSION=${BUILDER_GOLANG_VERSION} --build-arg CRYPTO_LIB=${FIPS_ENABLE} --push \
 --platform linux/arm64,linux/amd64 \
---tag ${IMG_REPO}/cert-manager-cainjector:${VERSION}-${SPECTRO_VERSION} . -f ./Dockerfile.fips --target cainjector
+--tag ${IMG_REPO}/cert-manager-cainjector:${VERSION}-$SPECTRO_VERSION . -f ./Dockerfile.fips --target cainjector
 
 docker buildx build --build-arg BUILDER_GOLANG_VERSION=${BUILDER_GOLANG_VERSION} --build-arg CRYPTO_LIB=${FIPS_ENABLE} --push \
 --platform linux/arm64,linux/amd64 \
---tag ${IMG_REPO}/cert-manager-acmesolver:${VERSION}-${SPECTRO_VERSION} . -f ./Dockerfile.fips --target acmesolver
+--tag ${IMG_REPO}/cert-manager-acmesolver:${VERSION}-$SPECTRO_VERSION . -f ./Dockerfile.fips --target acmesolver
 
